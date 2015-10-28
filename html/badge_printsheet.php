1b4ec175e68133ab32f03a98d37c65c7b448f73a
root <root@admin.epicscouts.org>
2015-10-28 03:11:16 -0400

    Sheets Finalized

--- html/badge_printsheet.php
+++ html/badge_printsheet.php
@@ -35,26 +35,20 @@ for ($x = 0; $x<=8; $x++) {
 	$b7 = "B0".$y++;
 	$b8 = "B0".$y++;
 }*/
-$testSheet = "Badges15-16";
-$badgelist=array("B001", "B002", "B003", "B004", "B005", "B006", "B007", "B008");
-MakePrintSheet($badgelist, $testSheet);
+$testSheet = "Badges_2015-2016_";
 $badgelist=array("B001", "B002", "B003", "B004", "B005", "B006", "B007", "B008");
 MakePrintSheet($badgelist, $testSheet."1");
-$badgelist=array("B001", "B002", "B003", "B004", "B005", "B006", "B007", "B008");
+$badgelist=array("B009", "B010", "B011", "B012", "B013", "B014", "B015", "B016");
 MakePrintSheet($badgelist, $testSheet."2");
-$badgelist=array("B009", "B010", "B011", "B012", "B013", "B014", "B0115", "B016");
+$badgelist=array("B017", "B018", "B019", "B020", "B021", "B022", "B023", "B024");
 MakePrintSheet($badgelist, $testSheet."3");
-$badgelist=array("B018", "B017", "B019", "B020", "B021", "B022", "B023", "B024");
-MakePrintSheet($badgelist, $testSheet."4");
 $badgelist=array("B025", "B026", "B027", "B028", "B029", "B030", "B031", "B032");
-MakePrintSheet($badgelist, $testSheet."5");
+MakePrintSheet($badgelist, $testSheet."4");
 $badgelist=array("B033", "B034", "B035", "B036", "B037", "B038", "B039", "B040");
-MakePrintSheet($badgelist, $testSheet."6");
+MakePrintSheet($badgelist, $testSheet."5");
 $badgelist=array("B041", "B042", "B043", "B044", "B045", "B046", "B047", "B048");
-MakePrintSheet($badgelist, $testSheet."7");
-
+MakePrintSheet($badgelist, $testSheet."6");
 $badgelist=array("B049", "B050", "B051", "B052", "B053", "B054", "B055", "B056");
-
-MakePrintSheet($badgelist, $testSheet."8");
+MakePrintSheet($badgelist, $testSheet."7");
 //}
 ?>
